73faf88968a16b695c07ceae62d26fa9d66779e4
nyawiradev <njaginjoyce@gmail.com>
2022-06-06 22:52:12 +0300

    commit tree bc6458870291ef9f031017316763197eb2c76be0

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+
+const max = Math.max(5,20,10,4);
